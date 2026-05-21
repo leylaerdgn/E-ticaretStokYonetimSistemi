eff9539f5ee5d30a44d304a60d6f05a0288bcb19
Leyla Erdoğan <erdoganleyla98@gmail.com>
2026-05-21 12:27:06 +0300

    Product ve ProductManager sınıfları eklendi

---
+++ src/eticaretstokyonetimsistemi/ProductManager.java
@@ -0,0 +1,57 @@
+
+package eticaretstokyonetimsistemi;
+import java.util.ArrayList;
+public class ProductManager {
+    private ArrayList<Product> products;
+    
+    public ProductManager(){
+        this.products=new ArrayList<>();
+    }
+    
+    //Ürün ekleme
+    public void addProduct(Product product){
+        products.add(product);
+    }
+    
+    public ArrayList<Product> getProducts(){
+        return products;
+    }
+    
+    //Ürünleri Listeleme
+    public void listProducts(){
+        for(Product product: products){
+            System.out.println(product.getId() + " - "
+            + product.getName() + " - "
+            + product.getPrice() +" TL - "
+            + product.getStock() + " adet");
+        }
+    }
+    //Ürün silme
+    public void deleteProduct(int id){
+        for(Product product: products){
+            if(product.getId()==id){
+                products.remove(product);
+                System.out.println("Ürün silindi.");
+                return;
+            }
+        }   
+    }
+    
+    //Üürn güncelleme
+    public void update(int id, String newName, double newPrice, int newStock){
+        for(Product product: products){
+            if(product.getId()==id){
+                product.setName(newName);
+                product.setPrice(newPrice);
+                product.setStock(newStock);
+                
+                System.out.println("Ürün güncellendi.");
+                return;
+            }
+        }
+        System.out.println("Bu id'ye sahip ürün bulunamadı.");
+    }
+    
+    
+    }
+    
---
+++ src/eticaretstokyonetimsistemi/notes.md
@@ -0,0 +1,7 @@
+## 1. Adım
+ilk olarak product dosyamı oluşturdum. burada ürünlerimi tutmak için birkaç bilgi 
+tanımladım.
+
+## 2. Adım
+Sonrasında productmanager dosyamı oluşturdum. Burada arraylist kavramını öğrendim ve
+ürün ekleme,silme,güncelleme metodu yazdım. 
